fb9369731b26fccca537544929ad8a0d3a87ad52
DevmanushRaky <rakesucluci@gmail.com>
2023-09-12 19:33:21 +0530

    user profile UI completed

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import avatar from "../assets/profile.png"
+import styles from "../styles/Username.module.css"
 
 export default function Username() {
     return (
         <div className='container mx-auto'>
-            <div className='flex justify-content-center items-center h-screen'>
-                <div className=''>
+            <div className='flex justify-center items-center h-screen'>
+                <div className={styles.glass}>
 
                     <div className='title flex flex-col items-center'>
                         <h4 className='text-5xl font-bold'> Hello again !!</h4>
@@ -16,12 +18,12 @@ export default function Username() {
 
                     <form className='py-1'>
                         <div className='profile flex justify-center py-4'>
-                            <img src="" alt='avatar' />
+                             <img src={avatar} className={styles.profile_img} alt='avatar' />
                         </div>
 
                         <div className='textbox flex flex-col items-center gap-6'>
-                            <input type="text" placeholder='Username' />
-                            <button type="submit"> Let's Go</button>
+                            <input type="text" placeholder='Username' className={styles.textbox}/>
+                            <button type="submit" className={styles.btn}> Let's Go</button>
                         </div>
 
                         <div className='text-center py-4'>
