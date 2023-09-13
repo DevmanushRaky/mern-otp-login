e71000f4c1ad3e909f6b21fcf7c012be374e79b3
DevmanushRaky <rakesucluci@gmail.com>
2023-09-13 13:21:34 +0530

    formik user profile

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,41 +1,59 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import avatar from "../assets/profile.png"
-import styles from "../styles/Username.module.css"
+import avatar from '../assets/profile.png';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { usernameValidate } from '../helper/validate'
+
+import styles from '../styles/Username.module.css';
 
 export default function Username() {
-    return (
-        <div className='container mx-auto'>
-            <div className='flex justify-center items-center h-screen'>
-                <div className={styles.glass}>
 
-                    <div className='title flex flex-col items-center'>
-                        <h4 className='text-5xl font-bold'> Hello again !!</h4>
-                        <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
-                            Explore more by connecting with us.
-                        </span>
-                    </div>
+  const formik = useFormik({
+    initialValues : {
+      username : ''
+    },
+    validate : usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit : async values => {
+      console.log(values)
+    }
+  })
 
-                    <form className='py-1'>
-                        <div className='profile flex justify-center py-4'>
-                             <img src={avatar} className={styles.profile_img} alt='avatar' />
-                        </div>
+  return (
+    <div className="container mx-auto">
 
-                        <div className='textbox flex flex-col items-center gap-6'>
-                            <input type="text" placeholder='Username' className={styles.textbox}/>
-                            <button type="submit" className={styles.btn}> Let's Go</button>
-                        </div>
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
 
-                        <div className='text-center py-4'>
-                            <span className='text-gray-500'> Not a Member <Link className="text-red-500" to="/register">Register Now </Link></span>
-                        </div>
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass}>
 
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Hello Again!</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+              Explore More by connecting with us.
+            </span>
+          </div>
 
+          <form className='py-1' onSubmit={formik.handleSubmit}>
+              <div className='profile flex justify-center py-4'>
+                  <img src={avatar} className={styles.profile_img} alt="avatar" />
+              </div>
 
-                    </form>
+              <div className="textbox flex flex-col items-center gap-6">
+                  <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username' />
+                  <button className={styles.btn} type='submit'>Let's Go</button>
+              </div>
 
-                </div>
-            </div>
+              <div className="text-center py-4">
+                <span className='text-gray-500'>Not a Member <Link className='text-red-500' to="/register">Register Now</Link></span>
+              </div>
+
+          </form>
+
         </div>
-    )
-}
+      </div>
+    </div>
+  )
+} 
