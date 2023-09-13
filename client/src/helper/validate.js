2616643582d0cbe799ee5b1c3fb1e2c3bf5960cb
DevmanushRaky <rakesucluci@gmail.com>
2023-09-13 16:31:36 +0530

    profile UI created

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,24 +1,24 @@
 import toast from 'react-hot-toast'
 
 /** validate login page username */
-export async function usernameValidate(values){
+export async function usernameValidate(values) {
     const errors = usernameVerify({}, values);
 
     return errors;
 }
 
 /** validate password */
-export async function passwordValidate(values){
+export async function passwordValidate(values) {
     const errors = passwordVerify({}, values);
 
     return errors;
 }
 
 /** validate reset password */
-export async function resetPasswordValidation(values){
+export async function resetPasswordValidation(values) {
     const errors = passwordVerify({}, values);
 
-    if(values.password !== values.confirm_pwd){
+    if (values.password !== values.confirm_pwd) {
         errors.exist = toast.error("Password not match...!");
     }
 
@@ -26,7 +26,7 @@ export async function resetPasswordValidation(values){
 }
 
 /** validate register form */
-export async function registerValidation(values){
+export async function registerValidation(values) {
     const errors = usernameVerify({}, values);
     passwordVerify(errors, values);
     emailVerify(errors, values);
@@ -35,20 +35,26 @@ export async function registerValidation(values){
 }
 
 
+/** validate profile page */
+export async function profileValidation(values) {
+    const errors = emailVerify({}, values);
+    return errors;
+}
+
 /** ************************************************* */
 
 /** validate password */
-function passwordVerify(errors = {}, values){
+function passwordVerify(errors = {}, values) {
 
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-    if(!values.password){
+    if (!values.password) {
         errors.password = toast.error("Password Required...!");
-    } else if(values.password.includes(" ")){
+    } else if (values.password.includes(" ")) {
         errors.password = toast.error("Wrong Password...!");
-    }else if(values.password.length < 4){
+    } else if (values.password.length < 4) {
         errors.password = toast.error("Password must be more than 4 characters long");
-    }else if(!specialChars.test(values.password)){
+    } else if (!specialChars.test(values.password)) {
         errors.password = toast.error("Password must have special character");
     }
 
@@ -57,10 +63,10 @@ function passwordVerify(errors = {}, values){
 
 
 /** validate username */
-function usernameVerify(error = {}, values){
-    if(!values.username){
+function usernameVerify(error = {}, values) {
+    if (!values.username) {
         error.username = toast.error('Username Required...!');
-    }else if(values.username.includes(" ")){
+    } else if (values.username.includes(" ")) {
         error.username = toast.error('Invalid Username...!')
     }
 
@@ -68,12 +74,12 @@ function usernameVerify(error = {}, values){
 }
 
 /** validate email */
-function emailVerify(error ={}, values){
-    if(!values.email){
+function emailVerify(error = {}, values) {
+    if (!values.email) {
         error.email = toast.error("Email Required...!");
-    }else if(values.email.includes(" ")){
+    } else if (values.email.includes(" ")) {
         error.email = toast.error("Wrong Email...!")
-    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         error.email = toast.error("Invalid email address...!")
     }
 
