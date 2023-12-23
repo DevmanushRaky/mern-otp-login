89c84d2e25bd3d7bfd734388f48f3bbd44cb3e32
DevmanushRaky <rakesucluci@gmail.com>
2023-12-23 13:36:36 +0530

    all pai function in client

---
+++ client/src/helper/helper.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+
+axios.defaults.baseURL=process.env.REACT_APP_SERVER_DOMAIN;
+
+
+//  make api request 
+
+// authenticate function 
+export async function authenticate(username) {
+    try {
+        return await axios.post('/api/authenticate', { username })
+    } catch (error) {
+        return { error: " Username doesn't exist..!!" }
+    }
+}
+
+// get user details
+export async function getUser({ username }) {
+    try {
+        const data = await axios.get(`/api/user/${username}`);
+        return data;
+    } catch (error) {
+        return { error: " Password does not matcch" }
+    }
+}
+
+// register the user 
+export async function registerUser(credentials) {
+    try {
+        const { data: { msg }, status } = await axios.post(`/api/register`, credentials)
+        let { username, email } = credentials;
+        //  send email
+        if (status === 201) {
+            await axios.post('/api/registerMail', { username, userEmail: email, text: msg })
+        }
+        return Promise.resolve(msg)
+    } catch (error) {
+        return Promise.reject({ error })
+    }
+}
+
+// login function
+export async function verifyPassword({ username, password }) {
+    try {
+        if (username) {
+            const { data } = await axios.post('/api/login', { username, password })
+            return Promise.resolve({ data });
+        }
+    } catch (error) {
+        return Promise.reject({ error: " Password doesn't match " })
+    }
+}
+
+// update user profile function 
+export async function updateUser(response) {
+    try {
+        const token = await localStorage.getItem('token');
+        const data = await axios.put('/api/updateuser', response, { headers: { "Authrization": `Bearer ${token}` } })
+
+        return Promise.resolve({ data })
+    } catch (error) {
+
+        return Promise.reject({ error: " couldn't update profile  " })
+    }
+}
+
+//  generate otp 
+export async function generateOTP(username) {
+    try {
+        const { data: { code }, status } = await axios.get('/api/generateOTP', { params: { username } });
+        //  send mail with otp
+        if (status === 201) {
+            let { data: { email } } = await getUser({ username });
+            let text = `Your Password Recovery OTP is ${code}. Verify and recover your password.`
+            await axios.post('/api/registerMail', { username, userEmail: email, text, subject: "Password Recoverty OTP" })
+        }
+        return Promise.resolve(code);
+
+    } catch (error) {
+        return Promise.reject({ error })
+    }
+}
+
+
+
+// verify OTP
+export async function verifyOTP(username, code) {
+    try {
+        const { data, status } = await axios.get('/api/verifyOTP', { params:{ username, code }})
+        return { data, status}
+} catch (error) {
+    return Promise.reject({ error })
+}
+}
+
+
+//  reset password 
+export async function resetPassword(username, password){
+    try {
+        const {data, status} = await axios.put('/api/resetpassword', {username, password})
+        return Promise.resolve({data, status})
+    } catch (error) {
+        return Promise.reject({ error })
+    }
+}
