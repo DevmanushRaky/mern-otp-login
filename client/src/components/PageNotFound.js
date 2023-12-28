23aa927abbc7618dc57b6e61a25e65f77d2ba27a
DevmanushRaky <rakesucluci@gmail.com>
2023-12-28 08:36:01 +0530

    page not found page created

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-red-500 mb-4">404 - Page Not Found</h1>
+        <p className="text-xl text-gray-600 mb-8">Oops! The page you are looking for might be under construction.</p>
+       
+      </div>
+    </div>
+  );
 }
