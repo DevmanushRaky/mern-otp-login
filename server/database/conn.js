e273d62b46b056cee86a4dbcd198e5e1d2e127ef
DevmanushRaky <rakesucluci@gmail.com>
2023-12-28 09:14:09 +0530

    change database connection environment variable

--- server/database/conn.js
+++ server/database/conn.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 import ENV from "../config.js";
 
 async function connect() {
+
+  // Use default environment variable or "default_mongo_uri" if not available
+const ATLAS_URI = process.env.ATLAS_URI || ENV.ATLAS_URI;
+
   try {
     mongoose.set('strictQuery', true);
     const db = await mongoose.connect(ENV.ATLAS_URI);
