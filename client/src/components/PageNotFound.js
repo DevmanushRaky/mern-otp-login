77aa24762f451e2c79d0073e2512df60d88c3f2d
DevmanushRaky <rakesucluci@gmail.com>
2023-12-29 08:56:57 +0530

    change ES^ to CommnoJs import to require

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+const React = require('react');
+const { Link } = require('react-router-dom');  // Updated import statement
 
 export default function PageNotFound() {
   return (
