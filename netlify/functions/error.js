cf2124b45675677197d67dc9a218d55d15be44dd
DevmanushRaky <rakesucluci@gmail.com>
2023-12-29 07:25:17 +0530

    change error page of netlify

--- netlify/functions/error.js
+++ netlify/functions/error.js
@@ -1,13 +1,26 @@
-// netlify/functions/error.js
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import PageNotFound from '../../client/src/components/PageNotFound';
+// // netlify/functions/error.js
+// import React from 'react';
+// import ReactDOMServer from 'react-dom/server';
+// import PageNotFound from '../../client/src/components/PageNotFound';
 
-exports.handler = async (event, context) => {
-  const html = ReactDOMServer.renderToString(<PageNotFound />);
+// exports.handler = async (event, context) => {
+//   const html = ReactDOMServer.renderToString(<PageNotFound />);
+//   return {
+//     statusCode: 404,
+//     body: html,
+//     headers: {
+//       'Content-Type': 'text/html',
+//     },
+//   };
+// };
+
+
+
+// netlify/functions/error.js
+exports.handler = async () => {
   return {
     statusCode: 404,
-    body: html,
+    body: '<html><body><h1>Custom 404 Page</h1><p>Sorry, the page you are looking for does not exist.</p></body></html>',
     headers: {
       'Content-Type': 'text/html',
     },
