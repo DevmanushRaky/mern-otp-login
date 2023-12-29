d77bf98a850ecb4b756637cc7448f235d4d51ed9
DevmanushRaky <rakesucluci@gmail.com>
2023-12-29 07:43:37 +0530

    change error page of netlify and folder structure

---
+++ client/netlify/functions/error.js
@@ -0,0 +1,28 @@
+// netlify/functions/error.js
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import PageNotFound from '../../src/components/PageNotFound';
+
+exports.handler = async (event, context) => {
+  const html = ReactDOMServer.renderToString(<PageNotFound />);
+  return {
+    statusCode: 404,
+    body: html,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  };
+};
+
+
+
+// // netlify/functions/error.js
+// exports.handler = async () => {
+//   return {
+//     statusCode: 404,
+//     body: '<html><body><h1>Custom 404 Page</h1><p>Sorry, the page you are looking for does not exist.</p></body></html>',
+//     headers: {
+//       'Content-Type': 'text/html',
+//     },
+//   };
+// };
